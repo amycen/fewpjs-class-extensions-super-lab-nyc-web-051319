be1e4327d6d7c8e9b0e44d34b6a5140e6c58d5c0
Amy Cen <amy.cen@blackrock.com>
2019-07-08 04:14:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ class Tree {
   }
 }
 
-class Deciduous {
+class Deciduous extends Tree{
   constructor(species, name) {
     super(species)
     this.name = name
+  }
+
+  static definition(){
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
